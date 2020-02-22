b0c0d5575f44275788d9533fffea8cfdb709020d
furrrmanov <pokerzatashil@mail.ru>
2020-02-23 02:44:54 +0300

    feat: Added solution code

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,30 @@
-module.exports = function getSeason(/* date */) {
+module.exports = function getSeason(date ) {
   throw 'Not implemented';
-  // remove line with error and write your code here
+  if(!date){
+    return 'Unable to determine the time of year!'
+    }
+    else
+    if(date1.hasOwnProperty('getMonth')) {
+    throw Error
+    }
+    
+    switch(date.getMonth()){
+    case 12:
+    case 1:
+    case 2:
+    return 'winter';
+    case 3:
+    case 4:
+    case 5:
+    return 'spring';
+    case 6:
+    case 7:
+    case 8:
+    return 'summer';
+    case 9:
+    case 10:
+    case 11:
+    return 'autumn';
+    }
+  
 };
