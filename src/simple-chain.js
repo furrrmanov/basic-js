dd14e1c7b97cd6037779c05ed0f94ca866b7fe97
furrrmanov <pokerzatashil@mail.ru>
2020-02-23 02:38:32 +0300

    feat: add solution code

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,37 @@
 const chainMaker = {
+  result:[],
+  str:'',
   getLength() {
     throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result=this.result.length
+    return this
   },
   addLink(value) {
     throw 'Not implemented';
-    // remove line with error and write your code here
+    if(!value){
+      this.result.push('')
+    }
+    else{
+      this.result.push(value)
+    }
   },
+
   removeLink(position) {
     throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result=this.result.filter((item)=>item!==position)
+    return this
   },
   reverseChain() {
     throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result=this.result.reverse()
   },
   finishChain() {
     throw 'Not implemented';
-    // remove line with error and write your code here
+    for(let i of this.result){
+      this.str+=`(${i})`
+    }
+    this.str=this.str.slice(1,-1)
+    return this
   }
 };
 
