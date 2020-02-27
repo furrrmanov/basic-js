dcb1fa0437b32ab3a0fe72d3d264ccd068d0b37c
furrrmanov <pokerzatashil@mail.ru>
2020-02-27 18:54:46 +0300

    feat: code editing

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,28 +1,24 @@
 module.exports = function transform(arr) {
-    throw 'Not implemented';
-    if(! Array.isArray(arr)){
-        return 'Error'
-        }
-        for(let i =0;i<arr.length;i++){
-        if(arr[i]=='--double-next'&&i!==arr.length-1){
-        return arr.join('').replace(arr[i],arr[i+1]).split('')
-        }
-        else if(arr[i]=='--double-next'&&i==arr.length-1){
-        return arr
-        }
-        
-        if(arr[i]=='--discard-prev'&&i!==0){
-        arr.splice(i-1,2,'')
-        return arr.join('').split('')
-        }
-        else if(arr[i]=='--discard-prev'&&i==0){
-        return arr
-        }
-        if(arr[i]=='--double-prev'&&i!==0){
-        return arr.join('').replace(arr[i],arr[i-1]).split('')
-        }
-        else if(arr[i]=='--double-prev'&&i==0){
-        return arr
-        }
-        }
+    if (!Array.isArray(arr)) {
+        throw new Error();
+      }
+    
+      if(arr.length==0){
+        return []
+      }
+    
+      let app = []
+      for (let i = 0; i < arr.length; i++) {
+         if (/^--(double|discard)-(next|prev)$/.test(arr[i]) ) {
+            if (arr[i] == "--double-next" & (i+1) < arr.length ) app.push(arr[i+1]);
+           
+            if (arr[i] == "--double-prev" & (i-1) >= 0 ) app.push(arr[i-1]);
+           
+            if (arr[i] == "--discard-prev") app.pop();
+         } else {
+           if (arr[i-1] !== "--discard-next") app.push(arr[i]);
+         }
+      }
+      return app
+
 };
