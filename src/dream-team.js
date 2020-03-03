e59352006ae04ae7f887fd57f991cfe4e4ebcf38
furrrmanov <pokerzatashil@mail.ru>
2020-03-03 13:45:31 +0300

    feat: fix solution code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,8 @@
 module.exports = function createDreamTeam(members){
-  throw 'Not implemented';
   if(!Array.isArray(members)){
     return false
   }
- return members.filter(item=>typeof(item)=="string").sort().map(i=>i[0].toUpperCase()).join('')
+
+ let str = members.filter(item=>typeof(item)=="string")
+ return str.map(i=>i.trim()[0].toUpperCase()).sort().join('')
 };
